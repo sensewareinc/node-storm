cbd07b5d6682c91abf286eac558b62879ccfb7c8
Michael Dellanoce <michael.dellanoce@gmail.com>
2014-08-11 13:45:06 -0400

    Handle errors automatically in basicbolt

--- test/basicbolt.spec.js
+++ test/basicbolt.spec.js
@@ -19,4 +19,18 @@ describe('basicbolt', function() {
 			}))
 	})
 
+	it('catches errors, logs them, and fails the tuple', function(done) {
+		es.readArray([{id: '1', tuple: ['test1']}])
+			.pipe(basicbolt(function(data) {
+				throw new Error('test')
+			}))
+			.pipe(es.writeArray(function(err, array) {
+				array.should.have.length(2)
+				array[0].command.should.eql('log')
+				array[0].msg.should.startWith('Error: test')
+				array[1].should.eql({command: 'fail', id: '1'})
+				done()
+			}))
+	})
+
 })
